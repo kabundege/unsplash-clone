5d920f25cafbbe65e2c7418c52e8a43d6408d4f5
kabundege <christophekwizera1@gmail.com>
2021-01-21 12:54:33 +0200

    first dpeloy

---
+++ server.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.use(express.static("public"))
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>console.log(`⚡️[server]: Server is running at https://localhost:${port}`))
